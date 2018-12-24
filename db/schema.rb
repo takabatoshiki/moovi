96892615146733a6194fb2747fe15713e989e053
takabatoshiki <takabatoshiki@takabariju-no-MacBook-Pro.local>
2018-12-24 18:55:30 +0900

    add ajax

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_24_082336) do
+ActiveRecord::Schema.define(version: 2018_12_24_092031) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2018_12_24_082336) do
     t.string "director"
     t.text "detail"
     t.string "open_date"
+    t.index ["title"], name: "index_products_on_title"
   end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
